cd295c08030438b62b0abe3259e2d069625a7e23
dzelenke7 <dzelenke@gmail.com>
2026-07-25 22:24:33 -0400

    Add files via upload

---
+++ tcr-arduino-sketch.ino
@@ -0,0 +1,60 @@
+#include <LiquidCrystal.h>
+                                                
+LiquidCrystal lcd(4, 6, 10, 11, 12, 13);
+
+#define div2  1                                   // the thermistor attach to 
+#define div1 0                                    // reference resistor
+#define R_ref1 33000                              
+#define R_ref2 33000
+const int B = 3950;                               // the beta of the thermistor
+const float R0 = 10000.0;                         // starting resistance
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(div1, INPUT);                           // to read voltage at the voltage divider of the test resistor
+  pinMode(div2, INPUT);                           // to read voltage at the voltage divider of the thermistor
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.clear();
+}
+
+void loop()
+{
+  // read thermistor value
+  long a = analogRead(div2);
+
+  float V_therm = a * (5.0 / 1023.0);                       
+
+  float V_out = analogRead(div1) * (5.0 / 1023.0);
+  
+  float R_test = (V_out * R_ref1) / (5.0 - V_out);
+
+  float R_therm = (R_ref2 * V_therm) / (5.0 - V_therm);
+
+  float tempC = B / (log(R_therm / R0) + B / 298.0) - 273.0;
+  float tempF = 1.8 * tempC + 32.0;
+
+
+
+  lcd.setCursor(0, 0);                                 // set the cursor to column 0, line 0
+  lcd.print("Temp: ");                                 // Print a message of "Temp: "to the LCD.
+                                                       // Print temp in Fahrenheit
+  lcd.print(tempF);
+                                                      
+  lcd.print(char(223));                                // print the degree symbol
+  lcd.print("F");
+                                                      
+  lcd.setCursor(0, 1);                                 // set the cursor to column 0, line 1
+  lcd.print("R_test:");                                
+  lcd.print(R_test);                                   // print the test resistors resistance to the LCD.
+
+  // Send data to py.
+  Serial.println(tempC);
+  Serial.println(R_test);
+  Serial.println(R_therm);
+
+  delay(200);                                          //wait for 100 milliseconds
+}
+
